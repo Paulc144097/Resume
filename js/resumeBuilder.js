cb98e58f186056f53cf1a772a963181e1c15e71c
Paulc144097 <paul.chua77@Yahoo.com>
2015-08-04 04:11:51 -0700

    Last changes, clean up

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -26,12 +26,16 @@ var bio = {
 	    $("#header").append(formattedbioPic);
 	    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
 	    $("#topContacts").append(formattedMobile);
+	    $("#footerContacts").append(formattedMobile);
 	    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
 	    $("#topContacts").append(formattedEmail);
+	    $("#footerContacts").append(formattedEmail);
 	    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
 	    $("#topContacts").append(formattedGithub);
+	    $("#footerContacts").append(formattedGithub);
 	    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 	    $("#topContacts").append(formattedLocation);
+	    $("#footerContacts").append(formattedLocation);
 
 	if(bio.skills.length > 0) {
 	    $("#header").append(HTMLskillsStart);
